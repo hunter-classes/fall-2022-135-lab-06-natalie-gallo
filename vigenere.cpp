7d082010df0a0d7344c21319ec9c4880af92b75b
Natalie Gallo <nataliegallo@Natalies-Air.fios-router.home>
2022-10-12 21:54:28 -0400

    task c complete?

--- vigenere.cpp
+++ vigenere.cpp
@@ -5,37 +5,40 @@
 //function
 //assume keyword is only lowercase letters a-z (97 - 122)
 std::string encryptVigenere(std::string plaintext, std::string keyword){
-  //read thru each character of keyword
-  //convert the character to its ascii interger and do int - 97(a) to find how many times need to shift
-  //set that value to rshift
-  //read plaintext and extract each character into a variable
-  //for the first character, shift according to the keyword's letter value
-  //store this character into a message variable
+  /*read thru each character of keyword
+  convert the character to its ascii interger and do int - 97(a) to find how many times need to shift
+  set that value to rshift
+  read plaintext and extract each character into a variable
+  for the first character, shift according to the keyword's letter value
+  store this character into a message variable*/
+  
   char key_c;
   int shift;
   char c;
   std::string message;
   int counter = 0;
-  bool read = true;
+  int i = 0;
 
-  while (read){
-  for (int i = 0; i < keyword.length(); i++){
-    if (counter == plaintext.length()){
-      read = false;
-    } else {
-      key_c = keyword[i];
-      shift = (int)key_c - 97;
-    }
-      c = plaintext[counter];
+  while (counter != plaintext.length()){ //execute until read thru all of plaintext
+    key_c = keyword[i]; //get i character from keyword
+    shift = (int)key_c - 97; //calculate what letter in the alphabet keyword char is
+    c = plaintext[counter]; //starting at 0, store character from plaintext into c
+    if (!isalpha(c)){ //if c is NOT a letter, dont modify it and move to the next character
+      message = message + c;
+      counter = counter + 1;
+    } else { //if c is a letter, shift the character accordingly, move to the next character of the text and keyword
       message = message + shiftChar(c, shift);
       counter = counter + 1;
+      i = i + 1;
+    }
+    if (i == keyword.length()){ //if the keyword has been read, start again/loop
+      i = 0;
     }
   }
   return message;  
 }
-  
-//rshift is the magnitude of right shift (0 <= rshift <= 25)
 
+//shiftChar unchanged!
 char shiftChar(char c, int rshift){
   int c_shift = 0;
   int dec_c = int(c);

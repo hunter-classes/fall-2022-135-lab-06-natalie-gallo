a91d7eb51a68b49ee340dab32454dcd3364de748
Natalie Gallo <nataliegallo@Natalies-Air.fios-router.home>
2022-10-13 18:24:12 -0400

    edited main

--- main.cpp
+++ main.cpp
@@ -10,17 +10,17 @@ int main()
   //task b
   std::string result;
   result = encryptCaesar("Hello, World!", 10);
-  std::cout << "Ciphertext: " << result << std::endl;
+  std::cout << "Caesar Ciphertext: " << result << std::endl;
 
   //task c
   result = encryptVigenere("Hello, World!", "cake");
-  std::cout << "Ciphertext: " << result << std::endl;
+  std::cout << "Vigenere Ciphertext: " << result << std::endl;
 
   //task d
   result = decryptCaesar("Rovvy, Gybvn!", 10);
-  std::cout << "Decrypted: " << result << std::endl;
+  std::cout << "Caesar Decrypted: " << result << std::endl;
   result = decryptVigenere("Jevpq, Wyvnd!", "cake");
-  std::cout << "Decrypted: " << result << std::endl;
+  std::cout << "Vigenere Decrypted: " << result << std::endl;
   
   return 0;
 }

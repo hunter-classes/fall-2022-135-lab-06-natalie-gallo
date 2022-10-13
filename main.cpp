14a7e6d6bdbf04b9c6a8edeecfc34d5f5bbdfa19
Natalie Gallo <nataliegallo@Natalies-Air.fios-router.home>
2022-10-12 21:58:57 -0400

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <string>
 
-#include "caesar.h"
+//#include "caesar.h"
+#include "vigenere.h"
 
 int main()
 {
+  //task b
   std::string result = encryptCaesar("Way to Go!", 5);
+  std::cout << "Ciphertext: " << result << std::endl;
+
+  //task c
+  std::string result = encryptVigenere("Hello, World!", "cake");
   std::cout << "Ciphertext: " << result << std::endl;
   
   return 0;
